715fb098d96c21e181fb20a88605f03eee3ab7a8
Nikhil Kumar <developernik18@gmail.com>
2022-09-27 16:31:37 +0530

    added sidebar and dashboard first row

---
+++ src/component/sidebar/LeftSideBar.jsx
@@ -0,0 +1,51 @@
+import './leftSideBar.css';
+import { Link } from 'react-router-dom';
+
+export default function LeftSideBar() {
+  return (
+    <nav className='sideBarMenu'>
+      <div className="sideBarMenuItem">
+        <Link to="/" className='sideBarMenuLink'>
+          <span>
+            <img src="/images/dashboard.svg" alt="Dashboard icon" className='sideBarMenuIcon' />
+          </span>
+          <span className="sideBarMenuLabel">
+            Dashboard
+          </span>
+        </Link>
+      </div>
+      <div className="sideBarMenuItem">
+        <Link to="/exams" className='sideBarMenuLink'>
+          <span>
+            <img src="/images/exams.svg" alt="Exam icon" className='sideBarMenuIcon' />
+          </span>
+          <span className="sideBarMenuLabel">
+            Exams
+          </span>
+        </Link>
+      </div>
+      <div className="sideBarMenuItem">
+        <Link to="/leader-board" className='sideBarMenuLink'>
+          <span>
+            <img src="/images/leaderboard.svg" alt="Dashboard icon" className='sideBarMenuIcon' />
+          </span>
+          <span className="sideBarMenuLabel">
+            Leader Board
+          </span>
+          
+        </Link>
+      </div>
+      <div className="sideBarMenuItem">
+        <Link to="/my-performance" className='sideBarMenuLink'>
+          <span>
+            <img src="/images/performance.svg" alt="Dashboard icon" className='sideBarMenuIcon' />
+          </span>
+          <span className="sideBarMenuLabel">
+            My Performance
+          </span>
+          
+        </Link>
+      </div>
+    </nav>
+  )
+}
